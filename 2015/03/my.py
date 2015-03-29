679f64b717b8c919584f141a36043a7eca70e48d
Angel Angelov <hextwoa@gmail.com>
2015-03-29 22:37:39 +0300

    add solution 03

---
+++ 2015/03/my.py
@@ -0,0 +1,60 @@
+from math import sqrt
+from itertools import count
+
+
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        yield a
+
+
+def is_prime(number):
+    if number == 1:
+        return False
+    if number == 2:
+        return True
+    return all(number % divisor for divisor in range(2, int(sqrt(number)) + 2))
+
+
+def primes():
+    return (number for number in count(1) if is_prime(number))
+
+
+def alphabet(*, code=None, letters=""):
+    if code == 'lat':
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+    elif code == 'bg':
+        letters = 'абвгдежзийклмнопрстуфхцчшщъьюя'
+    return (letter for letter in letters)
+
+
+def call_sequence(sequence_name, arguments, extra_definitions):
+    sequence_functions = {
+        'alphabet': alphabet,
+        'primes': primes,
+        'fibonacci': fibonacci
+    }
+
+    sequence_functions.update(extra_definitions)
+
+    return sequence_functions[sequence_name](**arguments)
+
+
+def intertwined_sequences(sequences, *, generator_definitions={}):
+    sequence_iterators = {}
+    for sequence in sequences:
+        sequence_arguments = dict(sequence)
+
+        sequence_name = sequence_arguments.pop('sequence')
+        length = sequence_arguments.pop('length')
+
+        if sequence_name not in sequence_iterators:
+            sequence_iterators[sequence_name] = call_sequence(
+                    sequence_name, sequence_arguments, generator_definitions
+            )
+
+        iterator = sequence_iterators[sequence_name]
+
+        for _ in range(length):
+            yield next(iterator)
